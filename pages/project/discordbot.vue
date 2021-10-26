2014b2dd8670c6fd73a08590488b1a7cc23bb706
ArvidWedtstein <71834553+ArvidWedtstein@users.noreply.github.com>
2021-10-26 15:03:20 +0200

    dfkdgdfg

---
+++ pages/project/discordbot.vue
@@ -0,0 +1,14 @@
+<template>
+   <div id='discordbot'>
+       <Navbar/>
+       <h1>discoooordbot</h1>
+       
+   </div>
+</template>
+
+<script>
+export default {
+   template: 'discordbot',
+   transition: 'slide-bottom'
+}
+</script>
